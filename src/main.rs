050a510063e5e9a3a95c4a72b1c808bc4a0f0245
Slarper <you@example.com>
2024-12-05 21:26:08 +0800

    Refactor main.rs to use scoped threads and collect threads' return value

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{thread::sleep, time::Duration};
 fn main() {
     use std::thread;
 
-    thread::scope(|s| {
+    let x = thread::scope(|s| {
         s.spawn(|| {
             for i in 0..10 {
                 sleep(Duration::from_millis(100));
@@ -11,13 +11,31 @@ fn main() {
             }
         });
 
-        s.spawn(|| {
+        let k = s.spawn(|| {
             for i in 0..5 {
-                sleep(Duration::from_millis(200));
+                sleep(Duration::from_millis(100));
                 println!("hello from another scoped thread: {}", i);
             }
+            114514
         });
+
+        
+
+        let b = s.spawn(|| {
+            println!("hello 111");
+            1919810
+        });
+
+
+        let m = k.join().unwrap();
+        println!("m: {}", m);
+        let n = b.join().unwrap();
+        println!("n: {}", n);
+
+        (m,n)
     });
+
+    println!("result: {}", x.0 + x.1);
 
     println!("Hello, world!");
 }
